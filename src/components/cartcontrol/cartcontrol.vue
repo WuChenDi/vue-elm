74df66b6bd7ff928a1950b3e02b3fc2f447c5fe3
ChenDi <996194720@qq.com>
2018-02-22 20:10:16 +0800

    cartcontrol组件

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cartcontrol"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    food: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
