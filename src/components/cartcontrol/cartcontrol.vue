2d4a4c48f4b451accdce3f38ff35b542abea8732
ChenDi <996194720@qq.com>
2018-02-23 22:18:34 +0800

    cartcontrol

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease" v-show="food.count>0"></div>
+    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add"></div>
   </div>
